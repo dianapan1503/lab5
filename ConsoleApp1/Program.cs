3b30e62b096de50affce360c6f2d74ad444df750
dianapan1503 <D.Panasenko.FIT.121.20@knute.edu.ua>
2021-11-09 17:30:17 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введіть висоту и ширину матриці: ");
+
+                int size1 = Convert.ToInt32(Console.ReadLine());
+                int size2 = Convert.ToInt32(Console.ReadLine());
+                int[,] massive = new int[size1, size2];
+
+                MatrixFirst matrixFirst = new MatrixFirst(size1, size2, massive, "Одинична матриця");
+                matrixFirst.Matrix1();
+                Console.Write("\n");
+                MartixSecond martixSecond = new MartixSecond(size1, size2, massive, "Верхня трикутна матриця");
+                martixSecond.Matrix2();
+            }
+            catch
+            {
+                Console.WriteLine("З'явилось виключення");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    public class Matrix
+    {
+        public int Size1 { get; set; }
+        public int Size2 { get; set; }
+        public int[,] AMatrix { get; set; }
+
+        public Matrix(int s1, int s2, int[,] matrix)
+        {
+            this.Size1 = s1;
+            this.Size2 = s2;
+            this.AMatrix = matrix;
+        }
+
+        public virtual void Matrix1()
+        {
+            int firstarg = AMatrix.GetLength(0);
+            int secondarg = AMatrix.GetLength(1);
+            for (int i = 0; i < firstarg; i++)
+            {
+                for (int j = 0; j < secondarg; j++)
+                {
+                    if (i == j)
+                    {
+                        AMatrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        AMatrix[i, j] = 0;
+                    }
+
+                    Console.Write(AMatrix[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+        public virtual void Matrix2()
+        {
+            int firstarg = AMatrix.GetLength(0);
+            int secondarg = AMatrix.GetLength(1);
+            for (int i = 0; i < firstarg; i++)
+            {
+                for (int j = 0; j < secondarg; j++)
+                {
+                    AMatrix[i, j] = 1;
+                }
+            }
+            for (int i = 0; i < firstarg; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < secondarg; j++)
+                {
+
+                    if (i <= j)
+                        Console.Write(AMatrix[i, j] + "\t");
+                    else
+                        Console.Write("0\t");
+                }
+            }
+        }
+    }
+    class MatrixFirst : Matrix
+    {
+        public string MName { get; set; }
+        public MatrixFirst(int s1, int s2, int[,] matrix, string mname)
+            : base(s1, s2, matrix)
+        {
+            this.MName = mname;
+        }
+
+        public override void Matrix1()
+        {
+            Console.WriteLine($"Matrix name = {MName}");
+            base.Matrix1();
+        }
+    }
+    class MartixSecond : Matrix
+    {
+        public string MName { get; set; }
+        public MartixSecond(int s1, int s2, int[,] matrix, string mname)
+            : base(s1, s2, matrix)
+        {
+            this.MName = mname;
+        }
+        public override void Matrix2()
+        {
+            Console.WriteLine($"Matrix name = {MName}");
+            base.Matrix2();
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введіть висоту конуса: ");
+                double height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введіть радіус конуса: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Conus conus = new Conus(height, radius);
+                conus.InfoMethod();
+
+                Console.WriteLine();
+                Console.WriteLine("Зрізаний конус:");
+                Console.WriteLine("Введіть радіус зрізаного конуса: ");
+                double r2 = Convert.ToDouble(Console.ReadLine());
+                Con2 con2 = new Con2(height, radius, r2);
+                con2.InfoMethod();
+            }
+            catch
+            {
+                Console.WriteLine("З'явилось виключення");
+            }
+            Console.ReadLine();
+
+        }
+    }
+    class Conus
+    {
+        public double Height { get; set; }
+        public double Radius { get; set; }
+
+        public Conus(double h, double r)
+        {
+            this.Height = h;
+            this.Radius = r;
+
+        }
+        public virtual double ConVolume()
+        {
+            double vol = (Math.PI * Math.Pow(Radius, 2) * Height) / 3;
+            return vol;
+        }
+        public double ConArea()
+        {
+            double area = Math.PI * Math.Pow(Radius, 2);
+            return area;
+        }
+        public virtual void InfoMethod()
+        {
+            Console.WriteLine($"Висота = {Height}, Радіус = {Radius}");
+            Console.WriteLine($"Об'єм = {ConVolume()} , Площа = {ConArea()} ");
+        }
+    }
+    class Con2 : Conus
+    {
+        public double Radius2 { get; set; }
+        public Con2(double r, double h, double r2)
+            : base(r, h)
+        {
+            this.Radius2 = r2;
+        }
+
+        public override double ConVolume()
+        {
+            double vol1 = Math.PI * Height * (Math.Pow(Radius, 2) + (Radius * Radius2) + Math.Pow(Radius2, 2));
+            return vol1;
+        }
+
+        public override void InfoMethod()
+        {
+            Console.WriteLine($"Радіус 2 = {Radius2}");
+            Console.WriteLine($"Об'єм = {ConVolume()}");
+        }
+    }
+
+}
